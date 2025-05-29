870c3e6c0cc7f0ee0f49af9c10523d3686320146
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-05-29 13:17:34 +0200

    created POST request for register user

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,17 @@
 import express from "express";
+import { createUser } from "../services/userServices";
 
 const router = express.Router();
 
 //registrera användare
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const newUser = await createUser(username, password);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+});
 
 export default router;
