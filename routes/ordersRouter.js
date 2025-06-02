210bafd77078665bf93f75c4b492bb7dae6284cc
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-06-02 14:28:00 +0200

    imported validation middleware and changed try catch into if else

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { createOrder, getAllOrders, getOrderByUserId } from "../services/ordersServices.js";
 import { removeCartById } from "../services/cartServices.js";
-import { validateUserId } from "../middlewares/validators.js";
+import { validateCartId, validateUserId } from "../middlewares/validators.js";
 
 const router = express.Router();
 
@@ -18,19 +18,16 @@ router.get("/", async (req, res, next) => {
 });
 
 // this creates a order from a cart
-router.post("/", async (req, res, next) => {
-  try {
-    const { cartId } = req.body;
-    if (!cartId) {
-      return next({ status: 400, message: "cartId is required" });
-    }
-    const order = await createOrder(cartId);
-    const result = await removeCartById(cartId);
+router.post("/", validateCartId, async (req, res, next) => {
+  const { cartId } = req.body;
+
+  const order = await createOrder(cartId);
+  if (order) {
+    await removeCartById(cartId);
     console.log(`Order created by user:${cartId}, their cart is removed`);
     return res.status(201).json({ success: true, order });
-  } catch (error) {
-    console.error(error.message);
-    next(error);
+  } else {
+    next({ status: 400, message: `Order could not be created` });
   }
 });
 
