68aa59b9865c98233d5926c1cde3c4ce81f4d646
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-06-03 12:05:01 +0200

    cart model cleared

--- models/cart.js
+++ models/cart.js
@@ -2,14 +2,12 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-// Items i cart ska se ut såhär
 const cartItemSchema = new Schema({
   prodId: { type: String, required: true },
   price: { type: Number, required: true, min: [0, "Price cannot be negative"] },
   qty: { type: Number, required: true },
 });
 
-// Alla users cart ska se ut såhär och cartId är samma som UserId
 const cartSchema = new Schema({
   cartId: {
     type: String,
